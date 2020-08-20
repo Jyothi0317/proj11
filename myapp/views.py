5cf89cb1fa576e30a19a7bda9389f809a335bc7c
Jyothi0317 <you@example.com>
2020-08-20 22:05:44 +0530

    third commit

--- myapp/views.py
+++ myapp/views.py
@@ -74,5 +74,24 @@ def img_display(request):
 from myapp import forms
 
 def builtin(request):
+    
+    if request.method=="POST":
+        form=forms.SampleForm(request.POST)
+        if form.is_valid():
+            first_name=form.cleaned_data.get('first_name')
+            last_name=form.cleaned_data.get('last_name')
+            email=form.cleaned_data.get('email')
+            phno=form.cleaned_data.get('phno')
+            pwd=form.cleaned_data.get('pwd')
+            birth_day=form.cleaned_data.get('birth_day')
+            birth_month=form.cleaned_data.get('birth_month')
+            birth_year=form.cleaned_data.get('birth_year')
+            gender=form.cleaned_data.get('gender')
+            image=form.cleaned_data.get('image')
+            languages=form.cleaned_data.get('languages')
+            store_image(image)
+            data=form.cleaned_data
+            return render(request,"display_data.html",context=data)
+            
     form=forms.SampleForm()
     return render(request,'builtin.html',{'form':form})
